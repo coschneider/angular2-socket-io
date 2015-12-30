a9e43be4348a427836b77748970c9b6549e55d71
Carsten Schneider <cs@mycable.de>
2015-12-30 13:29:58 +0100

    Added demo src files and gulp tasks

---
+++ demo/components/app/app.ts
@@ -0,0 +1,19 @@
+import {Component, ViewEncapsulation} from 'angular2/core';
+import {
+  RouteConfig,
+  ROUTER_DIRECTIVES
+} from 'angular2/router';
+// import {HTTP_PROVIDERS} from 'angular2/http';
+
+import {HomeCmp} from '../home/home';
+
+@Component({
+  selector: 'app',
+  templateUrl: './components/app/app.html',
+  encapsulation: ViewEncapsulation.None,
+  directives: [ROUTER_DIRECTIVES]
+})
+@RouteConfig([
+  { path: '/', component: HomeCmp, as: 'Home' },
+])
+export class AppCmp {}
